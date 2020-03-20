a4fca501946f6fca6b7a6f6b6dfe95d7940509b2
Anthony <824283752@qq.com>
2020-03-20 17:48:49 +0800

    [build] 1.0.3

--- release.sh
+++ release.sh
@@ -11,7 +11,7 @@ then
     # commit
     git add .
     git commit -m "[build] $VERSION"
-    npm version $VERSION --message "[release] $VERSION" --allow-same-version
+    npm version $VERSION --message "[release] $VERSION" 
     git push origin master
 
     # publish
